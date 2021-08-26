3dcdce96d410049b3195bd0688e196b86c51602c
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2021-08-26 22:42:13 +0530

    Add files via upload

---
+++ for_each_LOOP.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mahika.cj;
+
+/**
+ *
+ * @author Mahika Agrawal
+ */
+public class for_each_LOOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        int Arr[]={10, 20, 30, 40, 50};
+        for(int i:Arr)
+        {
+            System.out.println(i+" ");
+        }
+    
+    }
