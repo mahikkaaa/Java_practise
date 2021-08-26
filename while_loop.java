43e58a7a405f61bfe0d4204c458fe4634fc3c55c
Mahika Agrawal <80638498+mahikkaaa@users.noreply.github.com>
2021-08-26 22:42:54 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mahika.cj;
+
+/**
+ *
+ * @author Mahika Agrawal
+ */
+public class while_loop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i=0,sum=0;
+        while(i<=10)
+        {
+            sum=sum+i;
+            i++;
+        }
+        System.out.println("sum is "+ sum);
+        
+    }
+    
+}
